e8cdd0ef9e1d320d560cc7b21687a7462a41cd3e
Carlos Moroni <carlosmoroni123@gmail.com>
2023-11-01 18:43:41 -0500

    atividade em grupo subindo aplicação no vercel

--- sgcmapi/src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
+++ sgcmapi/src/main/java/br/ufac/sgcmapi/config/FiltroCors.java
@@ -17,7 +17,8 @@ public class FiltroCors {
         corsConfig.setAllowedOrigins(Arrays.asList(
             "http://127.0.0.1:5500",
             "http://localhost:4200",
-            "https://localhost:4200"));
+            "https://localhost:4200",
+            "https://sgcmwebacademy.vercel.app"));
         corsConfig.setAllowedMethods(Arrays.asList("*"));
         corsConfig.setAllowedHeaders(Arrays.asList("*"));
         UrlBasedCorsConfigurationSource configSource = new UrlBasedCorsConfigurationSource();
